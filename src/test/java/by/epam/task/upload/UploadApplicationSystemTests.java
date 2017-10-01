5b5ddd5cb5b348b6928e8099fd940f71995fa156
Aliaksandr Kot <aliaksandr.kot@gmail.com>
2017-10-01 14:05:23 +0300

    system test was added

---
+++ src/test/java/by/epam/task/upload/UploadApplicationSystemTests.java
@@ -0,0 +1,18 @@
+package by.epam.task.upload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UploadApplicationSystemTests
+{
+
+	@Test
+	public void systemTest() {
+		// Running of this test is initializing spring boot application and check all of configuration
+	}
+
+}
